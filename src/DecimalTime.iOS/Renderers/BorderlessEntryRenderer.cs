c6de81232fe2ebca2622684ee09368127743a832
Mauro Piccotti <mauro.piccotti@gmail.com>
2018-03-30 22:58:09 +0200

    .

---
+++ src/DecimalTime.iOS/Renderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using DecimalTime.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(BorderedEntryRenderer))]
+namespace DecimalTime.iOS.Renderers
+{
+    public class BorderedEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null) {
+                var view = (Element as Entry);
+                if (view != null) {
+                    Control.BorderRect(new CoreGraphics.CGRect(0, 0, Frame.Width, Frame.Height));
+                    Control.BorderStyle = UITextBorderStyle.Line;
+                }
+            }
+        }
+    }
+}
---
+++ src/DecimalTime.iOS/Renderers/ColorSelectorRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using DecimalTime.Forms.Controls;
+using DecimalTime.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ColorSelector), typeof(ColorSelectorRenderer))]
+namespace DecimalTime.iOS.Renderers
+{
+    public class ColorSelectorRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null) {
+                var view = (Element as ColorSelector);
+                if (view != null) {
+                    view.Focused += (object sender, FocusEventArgs e2) => {
+                        var ev = e2;
+                    };
+                }
+            }
+        }
+    }
+}
